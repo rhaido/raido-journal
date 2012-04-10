ad2ea8043fff9d9b9aa7e84c579a50788252ff22
Mikhail Grozak <mgrozak@abpc11619.cern.ch>
2012-04-10 14:24:05 +0200

    create_venv was written to work correctly with virtualenv; uwsgi startup was modified;

---
+++ create_venv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pip install virtualenv
+virtualenv venv
+. venv/bin/activate
+pip install web.py flask uwsgi
--- start-uwsgi.sh
+++ start-uwsgi.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-uwsgi --http 127.0.0.1:9090 --wsgi-file index.py --chdir ./ --check-static /home/maria/dev/raido-journal
+./venv/bin/uwsgi --pyhome ./venv --http 127.0.0.1:9090 --wsgi-file index.py --chdir ./ --check-static ./
+
+#uwsgi --http 127.0.0.1:9090 --wsgi-file index.py --chdir ./ --check-static /home/maria/dev/raido-journal
 #--touch-reload /home/maria/dev/raido-journal/touch.txt
