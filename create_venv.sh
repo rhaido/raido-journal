9bcbdaf56ad877e65c5611559ccb5e88dadc6222
Mike Grozak <mike.grozak@gmail.com>
2012-07-17 16:39:41 +0200

    update create_venv.sh script

--- create_venv.sh
+++ create_venv.sh
@@ -3,13 +3,13 @@
 #sudo pip install virtualenv
 virtualenv venv
 . venv/bin/activate
-pip install flask uwsgi flask-sqlalchemy sqlalchemy psycopg2
+pip install flask uwsgi flask-sqlalchemy sqlalchemy psycopg2 flask-wtf flask-markdown 
 
 # if rhelX and siblings, than
 # go to http://yum.postgresql.org/9.1/redhat, add to yum repos (for example, pg.repo)
 # and do 
 # yum install --enablerepo=pg postgresql91-server.x86_64 postgresql91-libs.x86_64 postgresql91-devel.x86_64 postgresql91.x86_64
-# 
+# yum install python-devel
 # then
 
 # createuser -P -R -S -D -E train_user
