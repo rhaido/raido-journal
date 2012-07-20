6fd36a4c0019224bdd8110cccb4a40572b8459bc
mike <mike@abpc11619.cern.ch>
2012-07-20 16:39:34 +0200

    uwsgi start line for raido-journal

--- start-uwsgi.sh
+++ start-uwsgi.sh
@@ -4,3 +4,4 @@
 
 #uwsgi --http 127.0.0.1:9090 --wsgi-file index.py --chdir ./ --check-static /home/maria/dev/raido-journal
 #--touch-reload /home/maria/dev/raido-journal/touch.txt
+#~raido/raido-journal/venv/bin/uwsgi --pyhome /apps/raido/raido-journal/venv/ --http 82.179.130.42:9090 --wsgi-file train_journal.py --callable app
