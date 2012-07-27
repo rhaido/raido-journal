0402ac1f52cd5d671e1b5af43a68fd3e64d83815
Mike Grozak <mike.grozak@gmail.com>
2012-07-27 17:43:29 +0200

    added gunicorn basic configuration

---
+++ gunicorn.py
@@ -0,0 +1,17 @@
+import os
+ 
+def numCPUs():
+  if not hasattr(os, "sysconf"):
+    raise RuntimeError("No sysconf detected.")
+  return os.sysconf("SC_NPROCESSORS_ONLN")
+  
+bind = "0.0.0.0:9000"
+workers = numCPUs() * 2 + 1
+backlog = 2048
+#worker_class ="sync"
+worker_class =  "gevent"
+debug = True
+daemon = True
+pidfile ="/tmp/gunicorn.pid"
+errorlog ="/tmp/gunicorn.log"
+
